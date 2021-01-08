7d21c61f41100b95b074e40883a0080e82534a25
addynguyen <49758703+addynguyen@users.noreply.github.com>
2021-01-08 18:28:58 +0700

    practical work 3

---
+++ USTHWeather/app/src/main/java/vn/edu/usthweather/ForecastFragment.java
@@ -0,0 +1,23 @@
+package vn.edu.usthweather;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ForecastFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_forecast, container, false);
+        v.setBackgroundColor(Color.rgb(0, 98, 155));
+        return v;
+    }
+}
--- USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
@@ -1,43 +1,51 @@
 package vn.edu.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private String TAG ="run";
+    private String TAG = "run";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        Fragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,"onStart");
+        Log.i(TAG, "onStart");
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"onResume");
+        Log.i(TAG, "onResume");
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,"onPause");
+        Log.i(TAG, "onPause");
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,"onStop");
+        Log.i(TAG, "onStop");
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"onDestroy");
+        Log.i(TAG, "onDestroy");
     }
 }
